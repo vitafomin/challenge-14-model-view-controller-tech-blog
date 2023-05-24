c68bd6188aeec45951a2ba09144ffb6d7487496a
Vita Fomin <vita.bukhalo@gmail.com>
2023-05-24 17:49:53 -0400

    added code to User model

---
+++ models/User.js
@@ -0,0 +1,65 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+const { update } = require("lodash");
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeCreate: async (newUserData) => {
+                newUserData.email = await newUserData.email.toLowerCase();
+                return newUserData;
+            },
+            beforeCreate: async (updatedUserData) => {
+                if (updatedUserData.password) {
+                    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                }
+                return updatedUserData;
+            },
+            beforeCreate: async (updatedUserData) => {
+                updatedUserData.email = await updatedUserData.email.toLowerCase();
+                return updatedUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
+    }
+);
+
+module.exports = User;
