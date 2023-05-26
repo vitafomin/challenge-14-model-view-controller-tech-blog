2c445f5c0b6dfeec9cb5f13d3db5944dac313e22
Vita Fomin <vita.bukhalo@gmail.com>
2023-05-26 12:16:59 -0400

    added code to userRoutes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+router.post("/signup", async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        })
+    }
+    catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email }});
+
+        if (!userData) {
+            res.status(400).send({ message: "Incorrect email or password" });
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).send({ message: "Incorrect email of password" });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.send({ user: userData, message: "Your logged in!" });
+        });
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.post("/logout", (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    }
+    else {
+        res.status(404).end()
+    }
+});
+
+module.exports = router;
