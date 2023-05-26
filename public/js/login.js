ec5b974635dc95e17368f636e7bd9fd6fdfad8cd
Vita Fomin <vita.bukhalo@gmail.com>
2023-05-26 13:56:04 -0400

    added code to login.js

---
+++ public/js/login.js
@@ -0,0 +1,18 @@
+const loginFormHandler = async (event) => {
+    const email = document.querySelector("#email-login").value.trim()
+    const password = document.querySelector("#password-login").value.trim();
+
+    if (email && password) {
+        const response = await fetch("/api/users/login", {
+            method: "POST",
+            body: JSON.stringify({ email, password }),
+        });
+
+        if (response.ok) {
+            document.location.replace("/dashboard");
+        }
+        else {
+            alert(response.statusText)
+        }
+    }
+};
