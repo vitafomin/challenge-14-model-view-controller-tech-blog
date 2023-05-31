53ef7a4b108490d2de48d02761b823a9e9c44c83
Vita Fomin <vita.bukhalo@gmail.com>
2023-05-31 18:35:50 -0400

    added signup homeroute and linked css file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -72,5 +72,14 @@ router.get("/login", (req, res) => {
         res.render("login");
     }
 });
+router.get("/signup", (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect("/dashboard");
+        return;
+    }
+    else {
+        res.render("signup");
+    }
+});
 
 module.exports = router;
